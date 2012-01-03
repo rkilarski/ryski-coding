44e9ce501bcf8a46869aad3cfad5ec5055524f48
rkilarski@gmail.com <rkilarski@gmail.com@68b9961e-1909-778e-dc18-8da7caa60cf9>
2012-01-03 20:01:54 +0000

    Progress on PDF and html adapters.

---
+++ guitariffic/src/com/guitariffic/dao/fakesheet/adapter/DOCX4JAdapter.java
@@ -0,0 +1,139 @@
+package com.guitariffic.dao.fakesheet.adapter;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.Vector;
+
+import javax.imageio.ImageIO;
+
+import org.docx4j.dml.wordprocessingDrawing.Inline;
+import org.docx4j.model.table.TblFactory;
+import org.docx4j.openpackaging.exceptions.InvalidFormatException;
+import org.docx4j.openpackaging.packages.WordprocessingMLPackage;
+import org.docx4j.openpackaging.parts.WordprocessingML.BinaryPartAbstractImage;
+import org.docx4j.wml.CTBorder;
+import org.docx4j.wml.STBorder;
+import org.docx4j.wml.Tbl;
+import org.docx4j.wml.TblBorders;
+import org.docx4j.wml.Tc;
+import org.docx4j.wml.Tr;
+
+import com.guitariffic.model.FakeSheet;
+import com.guitariffic.model.GuitarChordChart;
+
+public class DOCX4JAdapter
+{
+
+	public WordprocessingMLPackage generateDocument(FakeSheet fakeSheet)
+	{
+		WordprocessingMLPackage wordMLPackage;
+		try
+		{
+			wordMLPackage = WordprocessingMLPackage.createPackage();
+
+			// To get bold text, you must set the run's rPr@w:b,
+			// so you can't use the createParagraphOfText convenience method
+			org.docx4j.wml.ObjectFactory factory = new org.docx4j.wml.ObjectFactory();
+			org.docx4j.wml.P p = factory.createP();
+
+			// Add song and artist name.
+			org.docx4j.wml.Text t = factory.createText();
+			t.setValue(fakeSheet.getSongName() + "-" + fakeSheet.getArtistName());
+
+			org.docx4j.wml.R run = factory.createR();
+			run.getContent().add(t);
+
+			p.getContent().add(run);
+
+			org.docx4j.wml.RPr rpr = factory.createRPr();
+			org.docx4j.wml.BooleanDefaultTrue b = new org.docx4j.wml.BooleanDefaultTrue();
+			b.setVal(true);
+			rpr.setB(b);
+
+			run.setRPr(rpr);
+
+			wordMLPackage.getMainDocumentPart().addObject(p);
+
+			// Let's add a table
+			int writableWidthTwips = wordMLPackage.getDocumentModel().getSections().get(0).getPageDimensions().getWritableWidthTwips();
+			int cols = 2;
+			int cellWidthTwips = new Double(Math.floor((writableWidthTwips / cols))).intValue();
+
+			Tbl tbl = TblFactory.createTable(1, 2, cellWidthTwips);
+
+			// Set table borders
+			TblBorders borders = new TblBorders();
+			CTBorder border = new CTBorder();
+			border.setVal(STBorder.NONE);
+			borders.setLeft(border);
+			borders.setRight(border);
+			borders.setTop(border);
+			borders.setBottom(border);
+			borders.setInsideH(border);
+			borders.setInsideV(border);
+			tbl.getTblPr().setTblBorders(borders);
+
+			List<Object> rows = tbl.getContent();
+			Tr row = (Tr) rows.get(0);
+			List<Object> cells = row.getContent();
+			Tc tc = (Tc) cells.get(0);
+
+			// Lyrics
+			for (String text : fakeSheet.getTextArea().getTextAreaArray())
+			{
+				tc.getContent().add(wordMLPackage.getMainDocumentPart().createParagraphOfText(text));
+			}
+
+			// Images
+			tc = (Tc) cells.get(1);
+			Vector<Vector<GuitarChordChart>> chordCharts = fakeSheet.getChordChartArea().getChordChartArea();
+			for (Vector<GuitarChordChart> chartRow : chordCharts)
+			{
+				p = factory.createP();
+
+				for (GuitarChordChart chart : chartRow)
+				{
+					BinaryPartAbstractImage imagePart = BinaryPartAbstractImage.createImagePart(wordMLPackage, serializeObj(chart.getChordImage()));
+					Inline inline = imagePart.createImageInline(chart.getChordName(), chart.getChordName(), 0, 1, false);
+					run = factory.createR();
+					p.getContent().add(run);
+					org.docx4j.wml.Drawing drawing = factory.createDrawing();
+					run.getContent().add(drawing);
+					drawing.getAnchorOrInline().add(inline);
+				}
+				tc.getContent().add(p);
+			}
+
+			wordMLPackage.getMainDocumentPart().addObject(tbl);
+			return wordMLPackage;
+
+		} catch (InvalidFormatException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	private static byte[] serializeObj(BufferedImage obj)
+	{
+
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		try
+		{
+			ImageIO.write(obj, "jpg", baos);
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		byte[] bytesOut = baos.toByteArray();
+		return bytesOut;
+	}
+
+}
